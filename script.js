88c6eec7500d9828e781266270f17bc6b955444a
KimMurphy <9605070-KimMurphy@users.noreply.replit.com>
2023-12-23 15:30:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function enterCode(numeral) {
+  document.getElementById("code").innerHTML += numeral;
+  document.getElementById("message").innerHTML = "&nbsp";
+}
+
+function clearText() {
+  document.getElementById("code").innerHTML = "&nbsp;";
+  document.getElementById("message").innerHTML = "&nbsp";
+}
+function getCode() {
+  var num = document.getElementById("code").innerHTML;
+
+  if (num == "&nbsp;6495") {
+    document.getElementById("code").innerHTML = "";
+    document.getElementById("numbers").className = "hide";
+    document.getElementById("message").className = "correct";
+    document.getElementById("message").innerHTML = "<br><br><br><br>If you want to go to heaven<br>be sure to read Matthew 6:11";
+  }
+  else {
+    document.getElementById("code").innerHTML = "&nbsp;";
+    document.getElementById("message").className = "incorrect";
+    document.getElementById("message").innerHTML = "Incorrect Code";
+  }
+}
